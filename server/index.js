805ab87451aa4fed9a512eb1d528a9988dc807b2
Jil Rupani <jilrupani04@gmail.com>
2025-07-08 11:57:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,46 +1,29 @@
 import express from 'express';
+import bodyParset from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
-// import authRoutes from './routes/auth.js';
-import bookRoutes from './routes/books.js'
+
 const app = express();
-// app.use(bodyParser.json({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '50mb' })); // This is important!
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-
-
-// const app = express();
-
 dotenv.config();
-
-
-app.use(express.json());
 app.use(cors());
+
+
+app.use(bodyParset.json({ limit:"30mb",extended: true}));
+app.use(bodyParset.urlencoded({ limit:"30mb",extended: true}));
+app.use(express.json());
 app.use('/api/books', bookRoutes);
 app.use('/api/users', userRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-//   .catch((err) => console.log(err));
-
-  
+// const  CONNECTION_URL = "mongodb+srv://jilrupani04:jilrupani04@cluster0.7a7wjpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-    res.send('Hello to Books API.');
+    res.send('Hello to Memories API.');
 })
-// mongoose.connect(CONNECTION_URL)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
-
-// app.use('/user',userRoutes);
-
-mongoose.connect(process.env.MONGO_URI,{useNewURLParser : true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewURLParser : true, useUnifiedTopology: true})
     .then(() => { app.listen(PORT, () => console.log(`server running on port : ${PORT}`))   })
-    .catch((err)=>{ console.log(err);   
+    .catch((err)=>{ console.log(err);
 })
+
+// mongoose.set('useFindAndModify', false);
