285c4dd429446f9d1325714433061460a859ad40
Jil Rupani <jilrupani04@gmail.com>
2025-07-09 07:18:23 +0000

    second

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
-import bookRoutes from './routes/books.js'; // Added this
+import bookRoutes from './routes/books.js';
 
 dotenv.config();
 
@@ -23,13 +23,10 @@ app.get('/', (req, res) => {
   res.send('Hello to Memories API.');
 });
 
-const PORT = process.env.PORT || 5000;
-
+// No app.listen here
 mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-  })
-  .catch((err) => console.log(err));
+});
+
+export default app;
